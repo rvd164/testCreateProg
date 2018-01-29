38a13e9cc291ea8c0133b73f73b3717ddfc849fe
rvd <rdmytrush@gmail.com>
2018-01-29 12:57:43 +0200

    Initial Commit

---
+++ MyPrj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyPrj
+//
+//  Created by rvd on 1/29/18.
+//  Copyright © 2018 rvd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
