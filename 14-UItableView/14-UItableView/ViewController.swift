d01a1f811518901d8681900b6b5918f00deb150b
rvd <rdmytrush@gmail.com>
2018-02-09 22:53:42 +0200

    It's  create of array 3 elements

---
+++ 14-UItableView/14-UItableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  14-UItableView
+//
+//  Created by rvd on 2/9/18.
+//  Copyright © 2018 rvd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
