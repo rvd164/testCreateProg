47e68aa2f41ccc8c8b9bbfad806ed19774be4f38
rvd <rdmytrush@gmail.com>
2018-02-09 23:08:30 +0200

    ver 1.1 add functional delete elements

--- 14-UItableView/TableViewController.swift
+++ 14-UItableView/TableViewController.swift
@@ -32,6 +32,11 @@ class TableViewController: UITableViewController {
         return 1
     }
 
+    @IBAction func pushAddAction(_ sender: Any) {
+   dataArray.append("New element")
+        tableView.reloadData()
+    
+    }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return dataArray.count
@@ -50,25 +55,28 @@ class TableViewController: UITableViewController {
     
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
+
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            dataArray.remove(at: indexPath.row)
+            
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
